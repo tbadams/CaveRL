0e1ee4b81ef483b02b3f97eec5e9b396fc45a79e
Trevor B Adams <trevorbadams@gmail.com>
2013-02-09 14:56:12 -0800

    Initial Commit
    Halfway through part 2 of roguebasin tutorial.

---
+++ src/firstRL.py
@@ -0,0 +1,99 @@
+import libtcodpy as libtcod
+ 
+#actual size of the window
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+
+#map size
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+
+#colors
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_dark_ground = libtcod.Color(50, 50, 150)
+ 
+class Object:
+    #this is a generic object: the player, a monster, an item, the stairs...
+    #it's always represented by a character on screen.
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+ 
+    def move(self, dx, dy):
+        #move by the given amount
+        self.x += dx
+        self.y += dy
+ 
+    def draw(self):
+        #set the color and then draw the character that represents this object at its position
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+ 
+    def clear(self):
+        #erase the character that represents this object
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE) 
+ 
+def handle_keys():
+    global playerx, playery
+ 
+    #key = libtcod.console_check_for_keypress()  #real-time
+    key = libtcod.console_wait_for_keypress(True)  #turn-based
+ 
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #Alt+Enter: toggle fullscreen
+        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+ 
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return True  #exit game
+ 
+    #movement keys
+    if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+        player.move(0, -1)
+ 
+    elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+        player.move(0, 1)
+ 
+    elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+        player.move(-1, 0)
+ 
+    elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+        player.move(1, 0)
+ 
+ 
+#############################################
+# Initialization & Main Loop
+#############################################
+ 
+libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'CaveRL', False)
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+ 
+#create object representing the player
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+ 
+#create an NPC
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+ 
+#the list of objects with those two
+objects = [npc, player]
+ 
+while not libtcod.console_is_window_closed():
+ 
+    #draw all objects in the list
+    for object in objects:
+        object.draw()
+ 
+    #blit the contents of "con" to the root console and present it
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    libtcod.console_flush()
+ 
+    #erase all objects at their old locations, before they move
+    for object in objects:
+        object.clear()
+ 
+    #handle keys and exit game if needed
+    exit = handle_keys()
+    if exit:
+        break
