805d4879962bb7c8fbf87ed0a362cdfa4181e86b
Trevor B Adams <trevorbadams@gmail.com>
2013-02-09 21:06:34 -0800

    Added some classes/functions for dungeon mapping
    Likely these will be heavily modified or removed in our actual game, but
    rectangles will be good to have around at least, and it means can follow
    the tutorial more closely.

--- src/firstRL.py
+++ src/firstRL.py
@@ -1,4 +1,5 @@
 import libtcodpy as libtcod
+import math
  
 #actual size of the window
 SCREEN_WIDTH = 80
@@ -12,6 +13,8 @@ MAP_HEIGHT = 45
 FOV_ALGO = 0  #default FOV algorithm
 FOV_LIGHT_WALLS = True
 MAX_LIGHT_RADIUS = 10
+
+LIMIT_FPS = 20  #20 frames-per-second maximum
 
 #colors
 color_dark_wall = libtcod.Color(0, 0, 100)
@@ -44,7 +47,7 @@ class Object:
         #erase the character that represents this object
         libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
 
-#map
+#map stuff
 class Tile:
     #a tile of the map and its properties
     def __init__(self, blocked, block_sight = None):
@@ -55,17 +58,61 @@ class Tile:
         if block_sight is None: block_sight = blocked
         self.block_sight = block_sight
         
+class Rect:
+    #a rectangle on the map. used to characterize a room.
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+        
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+ 
+    def intersect(self, other):
+        #returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+        
+def create_room(room):
+    global map
+    #go through the tiles in the rectangle and make them passable
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
+            
+def create_h_tunnel(x1, x2, y):
+    global map
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+        
+def create_v_tunnel(y1, y2, x):
+    global map
+    #vertical tunnel
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+            
 def make_map():
     global map
  
-    #fill map with "unblocked" tiles
-    map = [[ Tile(False)
+    #fill map with "blocked" tiles
+    map = [[ Tile(True)
         for y in range(MAP_HEIGHT) ]
             for x in range(MAP_WIDTH) ]
-    map[30][22].blocked = True
-    map[30][22].block_sight = True
-    map[50][22].blocked = True
-    map[50][22].block_sight = True
+ 
+    #create two rooms
+    room1 = Rect(20, 15, 10, 15)
+    room2 = Rect(50, 15, 10, 15)
+    create_room(room1)
+    create_room(room2)
+    create_h_tunnel(25, 55, 23)
+    player.x = 25
+    player.y = 23
     
 def render_all():
     global fov_map, color_dark_wall, color_light_wall
@@ -143,11 +190,8 @@ con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 #create object representing the player
 player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
  
-#create an NPC
-npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
- 
 #the list of objects with those two
-objects = [npc, player]
+objects = [player]
 
 #generate map (at this point it's not drawn to the screen)
 make_map()
